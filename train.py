4400bf8db9cc8d3472f30b19a0e1587ac67014c7
alexnikko <UndeadNike@yandex.ru>
2022-06-19 19:41:17 +0300

    implement Sudo rm -rf model

--- train.py
+++ train.py
@@ -7,6 +7,9 @@ from src.utils import read_and_clean_meta, prepare_dataset_meta
 from src.transforms import Normalizer
 from src.datasets import VoxcelebDataset
 from src.models.conv_tasnet import TasNet
+from src.models.dual_path_rnn import DualRNNmodel
+from src.models.sepformer import SepformerWrapper
+from src.models.sudormrf import SuDORMRF
 from src.losses import Pit, Snr
 
 if __name__ == '__main__':
@@ -44,7 +47,36 @@ if __name__ == '__main__':
         print(mix.shape)
         print(src.shape)
         break
-    net = TasNet()
+    # net = TasNet()
+    # net = DualRNNmodel(256, 64, 128, bidirectional=True, norm='ln', num_layers=6)
+    # net = SepformerWrapper(
+    #     encoder_kernel_size=16,
+    #     encoder_in_nchannels=1,
+    #     encoder_out_nchannels=256,
+    #     masknet_chunksize=250,
+    #     masknet_numlayers=2,
+    #     masknet_norm="ln",
+    #     masknet_useextralinearlayer=False,
+    #     masknet_extraskipconnection=True,
+    #     masknet_numspks=2,
+    #     intra_numlayers=8,
+    #     inter_numlayers=8,
+    #     intra_nhead=8,
+    #     inter_nhead=8,
+    #     intra_dffn=1024,
+    #     inter_dffn=1024,
+    #     intra_use_positional=True,
+    #     inter_use_positional=True,
+    #     intra_norm_before=True,
+    #     inter_norm_before=True,
+    # )
+    net = SuDORMRF(out_channels=128,
+                   in_channels=512,
+                   num_blocks=16,
+                   upsampling_depth=4,
+                   enc_kernel_size=21,
+                   enc_num_basis=512,
+                   num_sources=2)
     optimizer = torch.optim.Adam(net.parameters(), lr=1e-4)
     loss_fn = Pit(Snr(20), eval_func='min')
     losses = []
